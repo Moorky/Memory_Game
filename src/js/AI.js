6b8475425cd3bd4b17102f2be2075fb6bc8a37e8
Moorky <moorkyofficial@gmail.com>
2022-08-17 17:00:09 +0200

    Added code documentation for AI Processing

--- src/js/AI.js
+++ src/js/AI.js
@@ -8,37 +8,55 @@ import SmartCard from "./SmartCard.js";
 class AI {
 
     /**
-     * Gets the content of the game
+     * Gets the content of the game and creates own environment based on the game environment to calculate its next move
      *
      * @param game object
      */
     constructor(game) {
+        // INITS
         this.game = game;
-
         this.smartCards = [];
         this.selectedCards = [];
         this.flipCounter = 0;
 
+        // Creates own array of cards similar to the board cards, just with more functionality
         this.createSmartCards();
     }
 
+    /**
+     * Finds all cards on the webpage, creates a SmartCard for each of them.
+     */
     createSmartCards() {
         document.querySelectorAll(".flip-card").forEach((e, i) => {
             this.smartCards.push(new SmartCard(this.game.board.getBoardCards()[i], e));
         });
     }
 
+    /**
+     * @param {int} index is the index at which the smartCards array should return its card
+     * @returns {*} smartCard instance
+     */
     getSmartCardByID(index) {
         return this.smartCards[index];
     }
 
+    /**
+     * This method is used for the AI to "save/remember" the selection the opponent has made.
+     *
+     * @param cardsID is an array of the IDs of the selected cards.
+     */
     getDataFromPlayersTurn(cardsID) {
         cardsID.forEach((e) => {
-            this.getSmartCardByID(e).setCardValue();
+            this.getSmartCardByID(e).incrementCardValue();
         });
         this.checkMatchFound(cardsID);
     }
 
+    /**
+     * Checks if the opponent has found a match with their selection and saves that information.
+     *
+     * @param cardsID is an array of the IDs of the selected cards.
+     */
     checkMatchFound(cardsID) {
         let matchCounter;
         cardsID.forEach((e) => {
@@ -52,12 +70,20 @@ class AI {
         });
     }
 
+    /**
+     * Decrements each SmartCard value after each turn to mimic forgetting where each card is.
+     */
     decrementSmartCardValues() {
         this.smartCards.forEach((e) => {
             e.decrementCardValue()
         });
     }
 
+    /**
+     * Finds all matches on the board and calculates the sum of the card values of each match.
+     *
+     * @returns {*[]} array of the sum of Card values of each match
+     */
     getSmartCardMatchValuesSum() {
         let valArray = [];
         for (let i = 0; i < this.game.board.getCardCount(); i++) {
@@ -75,6 +101,12 @@ class AI {
         return valArray;
     }
 
+    /**
+     * Finds all cards with the same Card ID (= match) at a specific Card ID.
+     *
+     * @param index is the specific Card ID.
+     * @returns {*[]} an array of the Cards with the same ID which build a match.
+     */
     getSmartCardMatchByIndex(index) {
         let match = [];
         this.smartCards.forEach((e) => {
@@ -85,6 +117,12 @@ class AI {
         return match;
     }
 
+    /**
+     * Finds a match with the highest sum of Card values (this mimics "confidently" remembering where a card match is)
+     *
+     * @param valArray array of the sum of Card values of each match
+     * @returns {boolean|*} false if it has not seen/does not remember a full set of cards that are matching, else returns the shared ID of the cards that build a match
+     */
     findMostKnownMatchIndex(valArray) {
         let highestVal = 0;
         let matchIndex;
@@ -97,6 +135,11 @@ class AI {
         return highestVal === 0 ? false : matchIndex;
     }
 
+    /**
+     * Finds the first card with the lowest Card value.
+     *
+     * @returns {*} the first card with the lowest Card value
+     */
     findMostUnknownCard() {
         let card;
         this.smartCards.forEach((e) => {
@@ -107,11 +150,19 @@ class AI {
         return card;
     }
 
+    /**
+     * This method first calculates if it knows a full set of cards that build a match,
+     * tries to select it by mimicking "remembering" and if it fails,
+     * selects a card that it remembers the least, to mimic increasing its total knowledge.
+     */
     calculateCardChoice() {
         this.checkMatchChoice();
         this.checkUnknownCardChoice();
     }
 
+    /**
+     * Checks if it knows a full set of cards that build a match, if it does, starts the process of selecting those cards.
+     */
     checkMatchChoice() {
         let mostKnownMatchIndex = this.findMostKnownMatchIndex(this.getSmartCardMatchValuesSum());
         if (mostKnownMatchIndex !== false) {
@@ -119,20 +170,29 @@ class AI {
         }
     }
 
+    /**
+     * Selects a card that it remembers the least, to mimic increasing its total knowledge.
+     */
     checkUnknownCardChoice() {
         if (this.flipCounter !== this.game.board.getCopyCount()) {
             for (let i = 0; i < this.game.board.getCopyCount() - this.flipCounter; i++) {
                 let mostUnknownCard = this.findMostUnknownCard();
                 this.selectedCards.push(mostUnknownCard);
-                mostUnknownCard.setCardValue();
+                mostUnknownCard.incrementCardValue();
             }
         }
     }
 
+    /**
+     * Calculating and processing the probability of remembering and selecting the right cards of the match.
+     * Will search for the most unknown card in another function, if this process does not meet the required card amount by failing the probability process.
+     *
+     * @param smartCardMatch is an array of a set of cards that build a match. In this case, the match with the highest value is used
+     */
     processMatchProbabilities(smartCardMatch) {
         for (let i = 0; i < smartCardMatch.length; i++) {
             if (this.processProbability(smartCardMatch[i].getCardValue())) {
-                smartCardMatch[i].setCardValue();
+                smartCardMatch[i].incrementCardValue();
                 this.selectedCards.push(smartCardMatch[i]);
                 this.flipCounter++;
             }
@@ -142,10 +202,21 @@ class AI {
         }
     }
 
+    /**
+     * Simple formula that will return:
+     * - true, if the given value is higher than a random number between (incl.) 0 and (excl.) 1
+     * - false, if the given value is lower than the random number
+     *
+     * @param val is a probability value that should range between 0 and 1. (0.75 = 75% chance)
+     * @returns {boolean} true, if val is higher, false if lower
+     */
     processProbability(val) {
         return !!val && Math.random() < val;
     }
 
+    /**
+     * This method consists of all steps the AI has to make to process its turn.
+     */
     makeTurn() {
         this.calculateCardChoice();
         console.log(this.getSmartCardMatchValuesSum());
@@ -155,11 +226,17 @@ class AI {
         this.preparePlayerTurn();
     }
 
+    /**
+     * Clears some class variables for the next turn.
+     */
     clearSelection() {
         this.flipCounter = 0;
         this.selectedCards = [];
     }
 
+    /**
+     * Calls some method from the game instance to prepare the turn for the opponent.
+     */
     preparePlayerTurn() {
         this.game.unselectSelectedCards();
         this.game.enableCard();
--- src/js/SmartCard.js
+++ src/js/SmartCard.js
@@ -1,4 +1,17 @@
+/**
+ * The SmartCard Class is a much better version of the original Card Class.
+ * Though, it's only use is for the AI to work properly and to mimic the memory of a human being.
+ * This is done by assigning each SmartCard a value between 0 and 1.
+ * 0 meaning the AI cannot remember where a specific card is (0%).
+ * 1 meaning the AI knows where a specific card is (100%).
+ */
 class SmartCard {
+    /**
+     * Creates a SmartCard!
+     *
+     * @param boardCard is an object from the Card class.
+     * @param htmlCard is a html element that contains the frontend Card information.
+     */
     constructor(boardCard, htmlCard) {
         this.boardCard = boardCard;
         this.htmlCard = htmlCard;
@@ -6,38 +19,65 @@ class SmartCard {
         this.matched = false;
     }
 
+    /**
+     * @returns {boolean} true if this card has already been matched, else false.
+     */
     getMatched() {
         return this.matched;
     }
 
+    /**
+     * Sets the matched variable for this card to true.
+     */
     setMatched() {
         this.matched = true;
     }
 
+    /**
+     * @returns {number} the value of the card (between 0 and 1).
+     */
     getCardValue() {
         return this.cardValue;
     }
 
-    setCardValue() {
+    /**
+     * Increments the value of the card with a specific formula.
+     */
+    incrementCardValue() {
         this.cardValue = this.cardValue > 0 ? 1 : 0.9;
     }
 
+    /**
+     * Decrements the value of the card with a specific formula.
+     */
     decrementCardValue() {
         this.cardValue *= 0.9;
     }
 
+    /**
+     * @returns {Number} the ID the card has which is usually saved in the Card class.
+     */
     getBoardCardID() {
         return this.boardCard.getID();
     }
 
+    /**
+     * @returns {string} the ID the card has which is usually saved in the html element of the card.
+     */
     getHtmlCardID() {
         return this.htmlCard.dataset.id;
     }
 
+    /**
+     * @returns {*} the Card instance.
+     */
     getBoardCard() {
         return this.boardCard;
     }
 
+    /**
+     * @returns {*} the html element of the card.
+     */
     getHtmlCard() {
         return this.htmlCard;
     }
