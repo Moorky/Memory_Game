797cc83dfd2b36ed664c8c26d2c4cb2840fd97ea
Moorky <moorkyofficial@gmail.com>
2022-08-18 12:03:39 +0200

    Changed code doc of AI

--- src/js/AI.js
+++ src/js/AI.js
@@ -3,7 +3,7 @@ import SmartCard from "./SmartCard.js";
 /**
  * This AI is used for the player vs computer mode.
  * It manipulates the game logic and overrides player 2.
- * It is completely dynamic and will make decisions based on the opponents skill and the overall card count & card copies.
+ * It is completely dynamic and mimics the memory and decision-making of a human being, based on knowledge and remembrance.
  */
 class AI {
 
